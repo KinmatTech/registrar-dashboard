cb505097e5d43ed24ca4e9c029067b40978c9e8b
Kinmat  Tech <mathew.ani@unn.edu.ng>
2023-05-11 23:45:35 +0100

    https://github.com/KinmatTech/registrar-dashboard.git

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,14 @@
-import Chart from '../components/Chart';
-import FeaturedInfo from '../components/FeaturedInfo'
+// import Chart from '../components/Chart';
 import '../css/page/home.css'
-import { userData } from "../dummyData";
-// import Footer from '../components/Footer';
-// import WidgetSm from '../components/WidgetSm';
-// import WidgetLg from '../components/WidgetLg';
+// import { userData } from "../dummyData";
+// import {Container, Row, Col} from 'react-bootstrap'
 
 function Home() {
   return (
     <div className="home">
-      <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-      <div className="homeWidgets">
-        {/* <WidgetSm/>
-        <WidgetLg/> */}
-      </div>
-      {/* <Footer /> */}
+      <h2 align="center">Welcome to Registrar Dashboard</h2>
+           
+      
     </div>
   );
 }
